6a9f53b5e1a3f5dd350a0ce24cdf92a705ac01d5
Chris Burnett <dameramu@gmail.com>
2014-05-09 01:40:35 +0100

    Fixing the ordering of sensitivity category cards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508235738) do
+ActiveRecord::Schema.define(version: 20140PPPP508235738) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
